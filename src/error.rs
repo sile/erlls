82abe962d4d17903ae2b37e461ca71c9742cd147
Takeru Ohta <phjgt308@gmail.com>
2023-07-06 18:39:33 +0900

    Update error type

--- src/error.rs
+++ src/error.rs
@@ -1,2 +1,59 @@
-#[derive(Debug, Clone)]
-pub enum ResponseError {}
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResponseError {
+    code: ErrorCode,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    message: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    data: Option<serde_json::Value>,
+}
+
+impl ResponseError {
+    pub fn parse_error() -> Self {
+        Self {
+            code: ErrorCode::PARSE_ERROR,
+            message: None,
+            data: None,
+        }
+    }
+
+    pub fn message(mut self, s: &str) -> Self {
+        self.message = Some(s.to_owned());
+        self
+    }
+
+    pub fn data(mut self, v: serde_json::Value) -> Self {
+        self.data = Some(v);
+        self
+    }
+}
+
+impl From<serde_json::Error> for ResponseError {
+    fn from(value: serde_json::Error) -> Self {
+        let line = value.line();
+        let column = value.column();
+        let category = value.classify();
+        let error = Self::try_from(value).expect("bug");
+        error.data(serde_json::json!({
+            "line": line,
+            "column": column,
+            "category": format!("{category:?}"),
+        }))
+    }
+}
+
+impl std::fmt::Display for ResponseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct ErrorCode(i32);
+
+impl ErrorCode {
+    pub const PARSE_ERROR: Self = Self(-32700);
+}
--- src/message.rs
+++ src/message.rs
@@ -1,15 +1,39 @@
 use crate::error::ResponseError;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug)]
+#[derive(Debug, Default, Clone, Copy, Serialize, Deserialize)]
+#[serde(try_from = "&str", into = "&str")]
+struct JsonrpcVersion;
+
+impl From<JsonrpcVersion> for &str {
+    fn from(_: JsonrpcVersion) -> Self {
+        "2.0"
+    }
+}
+
+impl TryFrom<&str> for JsonrpcVersion {
+    type Error = ResponseError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value == "2.0" {
+            Ok(Self)
+        } else {
+            Err(ResponseError::parse_error()
+                .message(&format!("Unsupported JSON-RPC version: {value}")))
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Message {
-    jsonrpc: &'static str,
+    jsonrpc: JsonrpcVersion,
 }
 
 impl Message {
     pub const JSONRPC_VERSION: &'static str = "2.0";
 
     pub fn from_bytes(buf: &[u8]) -> Result<Self, ResponseError> {
-        serde_json::from_slice(buf);
+        let msg: Message = serde_json::from_slice(buf)?;
         todo!()
     }
 }
