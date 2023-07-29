915e0d96e9472dbb7ac9a61f207676be8b4d83f8
Takeru Ohta <phjgt308@gmail.com>
2023-07-29 14:15:35 +0900

    Add LSP client side

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,26 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as path from 'path';
+import { ExtensionContext } from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient/node';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "erlls" is now active!');
+let client: LanguageClient;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('erlls.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from erlls!');
-	});
+export function activate(context: ExtensionContext) {
+    const serverModule = context.asAbsolutePath(
+        path.join('out', 'server.js')
+    );
 
-	context.subscriptions.push(disposable);
+    const serverOptions: ServerOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+        }
+    };
+
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: 'file', language: 'erlang' }]
+    };
+
+    client = new LanguageClient(
+        'erlls',
+        'Erlang Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    client.start();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
